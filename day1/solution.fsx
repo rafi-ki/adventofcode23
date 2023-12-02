c9bad86e4db9f80574dbf41d70d0357df18fa6aa
Rafael Teyral <r.teyral@tricentis.com>
2023-12-02 13:32:58 +0100

    give up sol2 on day1

--- day1/solution.fsx
+++ day1/solution.fsx
@@ -7,25 +7,45 @@ let input =
 
 let isDigit (x:Char) = Char.IsDigit x
 
-let numberChars = ["one"; "two"; "three"; "four"; "five"; "six"; "seven"; "eight"; "nine" ]
+let numberChars = ["one"; "two"; "three"; "four"; "five"; "six"; "seven"; "eight"; "nine"; "1"; "2"; "3"; "4"; "5"; "6"; "7"; "8"; "9"; "10"; ]
 
+let mapNumbers number = match number with
+                        | "one" -> "1"
+                        | "two" -> "2"
+                        | "three" -> "3"
+                        | "four" -> "4"
+                        | "five" -> "5"
+                        | "six" -> "6"
+                        | "seven" -> "7"
+                        | "eight" -> "8"
+                        | "nine" -> "9"
+                        | x -> x
 
-let digits (line: string) =
-    let first = line |> Seq.find isDigit
-    let last = line |> Seq.findBack isDigit
-    let digits = first.ToString() + last.ToString()
+// let digits (line: string) =
+//     let first = line |> Seq.find isDigit
+//     let last = line |> Seq.findBack isDigit
+//     let digits = first.ToString() + last.ToString()
+//     digits |> int
+
+let matches (line: string) (number: string) = line.Contains number
+
+let matchesFor (line: string) (numbers: string list) =
+    let matchingNumbers = numbers |> List.filter (matches line)
+    let indexesOfMatchingNumbers = matchingNumbers |> List.map (fun x -> (line.IndexOf x, x))
+    let result = indexesOfMatchingNumbers |> List.sortBy fst
+    let firstDigit = result |> List.head |> snd |> mapNumbers
+    let lastDigit = result |> List.last |> snd |> mapNumbers
+    Console.WriteLine(firstDigit + lastDigit)
+    let digits = firstDigit.ToString() +  lastDigit.ToString()
     digits |> int
 
-let digitsFromText (line: string) =
-    let first = line |> Seq.find isDigit
-    let last = line |> Seq.findBack isDigit
-    let digits = first.ToString() + last.ToString()
-    digits |> int
+let digitsFromText (line: string) = matchesFor line numberChars
 
-let solution1 =
-    input
-    |> Array.map digits
-    |> Array.sum
+
+// let solution1 =
+//     input
+//     |> Array.map digits
+//     |> Array.sum
 
 let solution2 =
     input
