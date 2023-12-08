1117ec4411995493dbd398fa61a8c4507b80ba3d
Rafael Teyral <r.teyral@tricentis.com>
2023-12-08 20:45:31 +0100

    day 7 p1 done

--- day7/solution.fsx
+++ day7/solution.fsx
@@ -1,16 +1,25 @@
 open System
 open System.IO
 
-type Card = char
-
 type HandType = Five = 0 | Four = 1 | FullHouse = 2 | Three = 3 | TwoPair = 4 | OnePair = 5 | Highest = 6
 
-type Hand = { Card: string }
+type BidHand = { Hand: string; Bid: int; Type: HandType; Rank: int }
 
-type BidHand = { Hand: string; Bid: int; Type: HandType; Rank: int option }
+let inline charToInt c = int c - int '0'
+
+let mapCard (c: char) =
+    if Char.IsDigit c then charToInt c
+    else
+    match c with
+    | 'T' -> 10
+    | 'J' -> 11
+    | 'Q' -> 12
+    | 'K' -> 13
+    | 'A' -> 14
+    | _ -> failwith "impossible"
 
 let input =
-    let text = File.ReadAllText "example.txt"
+    let text = File.ReadAllText "input.txt"
     text.Split Environment.NewLine
 
 let parseType (hand: string) =
@@ -31,14 +40,28 @@ let parse (line: string) =
     let handString = split[0]
     let handType = parseType handString
     let bid = split[1] |> int
-    let h = { Hand = handString; Bid = bid; Rank = None; Type = handType }
-    printfn $"%A{h}"
-    h
+    { Hand = handString; Bid = bid; Rank = -1; Type = handType }
+
+let sort (bidHand1: BidHand) (bidHand2: BidHand) =
+    if bidHand1 = bidHand2 then 0
+    else if bidHand1.Type <> bidHand2.Type then bidHand1.Type.CompareTo bidHand2.Type
+    else
+        let result = Seq.zip bidHand1.Hand bidHand2.Hand
+                            |> Seq.skipWhile (fun (a, b) -> a = b)
+                            |> Seq.head
+        mapCard (snd result) - mapCard (fst result)
+
+let rank (bidHands: BidHand array) =
+    bidHands
+    |> Array.sortWith sort
+    |> Array.rev
+    |> Array.mapi (fun i hand -> { hand with Rank = i + 1 })
 
 let solution1 =
-    input
-    |> Array.map parse
-    |> Array.length
+    let ranked = input |> Array.map parse |> rank
+    ranked
+    |> Array.map (fun x -> x.Bid * x.Rank)
+    |> Array.sum
 
 let solution2 =
     2
