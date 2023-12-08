64241a8eea899c09c3b3010e5644a7e2ea5f0f07
Rafael Teyral <r.teyral@tricentis.com>
2023-12-08 13:33:26 +0100

    day6 done

--- day6/solution.fsx
+++ day6/solution.fsx
@@ -3,15 +3,14 @@ open System.IO
 
 type Race =
     {
-        Time: int;
-        Distance: int;
+        Time: int64;
+        Distance: int64;
     }
 
-
-let input = File.ReadAllText "example.txt"
+let input = File.ReadAllText "input.txt"
     // text.Split Environment.NewLine
 
-let mapToRace tupleArray = tupleArray |> Array.map (fun (t, d) -> { Time = t |> int; Distance = d |> int })
+let mapToRace tupleArray = tupleArray |> Array.map (fun (t, d) -> { Time = t |> int64; Distance = d |> int64 })
 
 let parse (text: string) =
     let lines = text.Split Environment.NewLine
@@ -21,8 +20,19 @@ let parse (text: string) =
     let distances = distanceLine[1].Split(' ', StringSplitOptions.RemoveEmptyEntries)
     mapToRace (Array.zip times distances)
 
+let calcDistance (time: int) (duration: int) : int64=
+    if duration = 0 then 0L
+    else int64 (time - duration) * (int64 duration)
+    // 0 -> 0
+    // 1 -> time-1
+    // 2 -> time-2 * 2
+    // 3 -> time-3 * 3
+
 let countWins (race: Race) =
-    2
+    let tada = calcDistance (int race.Time)
+    let possibilities = [|0.. (int race.Time)|] |> Array.map tada
+    printfn $"%A{possibilities}"
+    possibilities |> Array.filter (fun x -> race.Distance < x) |> Array.length
 
 let solution1 =
     input
@@ -30,11 +40,9 @@ let solution1 =
     |> Array.map countWins
     |> Array.fold (fun total n -> total * n) 1
 
-// let solution2 =
-//     input
-//     |> Array.map parseToGame
-//     |> Array.map mapToMaxBag
-//     |> Array.map multiplySetOfCubes
-//     |> Array.sum
+let solution2 =
+    // let race = { Time = 71530; Distance = 940200 }
+    let race = { Time = 40817772; Distance = 219101213651089L }
+    countWins race
 
-let run = printf $"solution: %i{solution1}"
+let run = printf $"solution: %i{solution2}"
